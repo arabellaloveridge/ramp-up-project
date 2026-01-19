48889e257e0e7383e91eac56e3a483034b538060
ARALOVER <arabellaloveridge@gmail.com>
2026-01-19 13:20:36 -0500

    add content to page

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const textToChange = document.getElementById("text-to-change")
+const image = document.getElementById("image")
 
+image.addEventListener("click", function() {
+    alert("Nice work!");
+})
